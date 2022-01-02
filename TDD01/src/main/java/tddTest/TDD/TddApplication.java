53030e627d3af4feb9158f20970af44ede186f86
yuhwanwoo <6514687@naver.com>
2022-01-02 19:20:11 +0900

    TDD 테스트 1

---
+++ TDD01/src/main/java/tddTest/TDD/TddApplication.java
@@ -0,0 +1,14 @@
+package tddTest.TDD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddApplication.class, args);
+	}
+
+}
---
+++ TDD01/src/test/java/tddTest/TDD/TddApplicationTests.java
@@ -0,0 +1,72 @@
+package tddTest.TDD;
+
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+
+
+import static io.restassured.RestAssured.given;
+
+/*
+시나리오
+- 리뷰를 조회할 수 있게 해주세요.
+- 리뷰 작성작에게 배민 선물하기 기능 추가해주세요.
+
+요구사항
+*후기 조회 하기 API*
+- [ ] 후기의 ID를 요청 값으로 받습니다.
+- [ ] 요청 ID에 대한 후기를 찾아 응답값으로 내려줍니다. (200 OK)
+- [ ] 응답에는 반드시 본문과 연락처를 포함해야 합니다.
+- [ ] 후기가 존재하지 않는다면 에러를 응답해야 합니다. (404 Not Found)
+*후기 작성자에게 선물하기 API*
+- [ ] 후기의 ID를 요청 값으로 받습니다.
+- [ ] 선물은 후기당 한번만 요청할 수 있습니다.
+- [ ] 선물하기에 선공하면 후기의 현재 상태를 응답합니다. (200 OK)
+- [ ] 선물하기는 아래의 API를 호출하여 수행합니다.
+ */
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class TddApplicationTests {
+
+	@LocalServerPort
+	int port;
+
+	@BeforeEach
+	void setUp() {
+		RestAssured.port = port;
+	}
+
+	@Test
+	void 후기_조회_성공() {
+		// given, arrange, 준비
+		given()
+				.accept(MediaType.APPLICATION_JSON_VALUE)
+		// when, act, 실행
+				.when()
+				.get("/reviews/1")
+		// then, assert, 검증
+				.then()
+				.statusCode(HttpStatus.OK.value())
+				.assertThat()
+				.body("id", Matchers.equalTo(1))
+				.body("content", Matchers.equalTo("재밌어요"))
+				.body("phoneNumber", Matchers.equalTo("010-1111-2222"));
+	}
+
+	@Test
+	void 후기_조회_실패() {
+
+	}
+
+	@Test
+	void 선물하기_성공() {
+
+	}
+
+
+
+}
